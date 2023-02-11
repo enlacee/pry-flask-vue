80e2eaee2e945023becca16c6fd6a5f3d34aff6a
Anibal Copitan <github@pprios.com>
2023-02-11 12:11:21 -0500

    [add] creating nuxt application and adding TheIntro.vue component

---
+++ flask/frontend/components/TheIntro.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="relative bg-gray-100 sm:items-center sm:pt-0">
+
+    <h2 class="text-2xl leading-7 font-semibold">
+      {{ message }}
+    </h2>
+
+    <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
+      To get started, remove
+      <code class="bg-gray-100 text-sm p-1 rounded border"
+        >components/TheIntro.vue</code
+      >
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      message: 'New message from VueJS'
+    }
+  },
+  created() {
+    this.getMessage();
+  },
+  methods: {
+    async getMessage() {
+      const url = 'http://localhost:5000/api/v1.0/message';
+      const rs = await fetch(url)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+          return response.json();
+        });
+
+      this.message = rs.message;
+    }
+  }
+}
+</script>
---
+++ flask/frontend/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <TheIntro />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
