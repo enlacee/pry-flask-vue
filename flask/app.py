7b2d014527aabd037957511258f251bedded5d25
Anibal Copitan <github@pprios.com>
2023-02-11 12:07:42 -0500

    [update] backend server is configured to CORS

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
+from datetime import datetime
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
 
-@app.route('/')
-def hello_geek():
-    return '<h1>Hello from Flask & Docker x2</h2>'
+app = Flask(__name__,
+    static_folder='./frontend/dist/nuxt',
+    template_folder='./frontend/dist')
 
+cors = CORS(app, resources={"/api/*": {"origins": "*"}})
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def render_vue(path):
+    return render_template('index.html')
+
+@app.route('/api/v1.0/message')
+def message():
+    data = {
+        "message": "Nuevo mensaje desde un servidor Flask = " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
